1fbb20609ca513c28e7b7ab3252e472bd9304999
xaouxaou009-debug <xaouxaou009@gmail.com>
2026-05-31 11:21:12 +0700

    Scripts

---
+++ QucikBuild.lua
@@ -0,0 +1,13 @@
+﻿local Build = GameMain:GetMod("QuickBuild");
+local time = 0;
+local flag = 0;
+
+function Build:OnStep(dt)
+if flag == 0 then
+time = time + dt;
+if time >= 10  then
+flag = 1;
+CS.GameMain.Instance.QuickBuild = true
+end
+end
+end
